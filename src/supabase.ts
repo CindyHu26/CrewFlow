c430e670bb8bfac32852e0644e5b2d96a3d78652
CindyHu26 <huwanhsin@gmail.com>
2025-05-17 22:46:29 -0400

    refactor: move supabase key to .env file

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lfkzvyaoecpqizykgory.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxma3p2eWFvZWNwcWl6eWtnb3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1MjY2NzIsImV4cCI6MjA2MzEwMjY3Mn0.tQQMg2vTkhzqSG9He-5bmu0zI3GIoJMijFNt9u1lp5I'; 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_UR;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
